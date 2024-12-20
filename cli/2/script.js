4c8897f65427538da580e00d877ff66e1785709b
Amiss888 <168334827+Amiss888@users.noreply.github.com>
2024-12-20 14:50:21 +0700

    Add files via upload

---
+++ cli/2/script.js
@@ -0,0 +1,25 @@
+let rock = document.querySelector("#rock")
+let paper = document.querySelector("#paper")
+let scissor = document.querySelector("#scissor")
+
+rock.addEventListener('click',() =>{
+let x = 1
+})
+
+paper.addEventListener('click',() =>{
+    let x = 2
+})
+scissor.addEventListener('click',() =>{
+    let x = 3
+})
+
+if(x == i) {
+    console.log("d")
+}
+else if(x > i) {
+console.log("w")
+}else if(x ) {
+    console.log("l")
+}
+
+console.log(Math.floor(Math.random() * 3));
